7ad4dbc3eaf2f62adc672f9c58d79aba6ce659ea
Dominique Unruh <unruh@ut.ee>
2021-09-05 20:58:25 +0300

    Added thms to Complex_Order

--- Complex_Order.thy
+++ Complex_Order.thy
@@ -12,6 +12,12 @@ instance
   by (auto simp: less_complex_def less_eq_complex_def complex_eq_iff)
 end
 
+lemma nonnegative_complex_is_real: \<open>(x::complex) \<ge> 0 \<Longrightarrow> x \<in> \<real>\<close>
+  by (simp add: complex_is_Real_iff less_eq_complex_def)
+
+lemma complex_is_real_iff_compare0: \<open>(x::complex) \<in> \<real> \<longleftrightarrow> x \<le> 0 \<or> x \<ge> 0\<close>
+  using complex_is_Real_iff less_eq_complex_def by auto
+
 instance complex :: ordered_comm_ring
   apply standard
   by (auto simp: less_complex_def less_eq_complex_def complex_eq_iff mult_left_mono mult_right_mono)
